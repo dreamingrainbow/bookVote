224def726d35e07b9d29fe1f1f4a3a70b5226e16
ubuntu <irishseanvaldivia@gmail.com>
2018-01-04 17:41:17 -0600

    Separated my components into subdirectories.

---
+++ bookvote/src/components/homepageDisplay/homepageDisplay.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePageDisplay extends Component {
+  render() {
+    return (
+      <div className="HomePageDisplay">
+
+      </div>
+    )
+  }
+}
+
+export default HomePageDisplay;
