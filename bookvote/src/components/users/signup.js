524370bfc2c6d7788418a5f053bf122ce445466e
analogy <actweed47@gmail.com>
2018-01-05 00:31:44 -0500

    removed fluff

--- bookvote/src/components/users/signup.js
+++ bookvote/src/components/users/signup.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { axios } from 'axios';
-import { withRouter, NavLink } from 'react-router-dom';
+import { withRouter} from 'react-router-dom';
 //import SignIn from './sign-in/signin.js';
 
 class SignUp extends Component {
