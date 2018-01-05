c68cc05a078567671be5016af6584dd714d5f84e
Frank Faustino <nawzyah@gmail.com>
2018-01-05 08:43:56 -0800

    Fixed keys for SearchResult components

--- bookvote/src/components/search/SearchResult.js
+++ bookvote/src/components/search/SearchResult.js
@@ -1,42 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './SearchResult.css';
 
-const SearchResult = props => {
-  const book = props.results;
-  let bookTitle = book.TITLE;
-  let bookAuthor = book.AUTHOR;
-  let bookISBN = book.ISBN;
-  let bookImage ='https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Clrs3.jpeg/220px-Clrs3.jpeg';
-  let upVote = book.VOTES.UP;
-  let downVote = book.VOTES.DOWN;
+class SearchResult extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      upVote: null,
+      downVote: null,
+      response: null
+    };
 
-  return (
-    <div className="searchResultContainer">
-      <div className="imageContainer">
-        <img src={bookImage} alt=""/>
-      </div>
-      <div className="bookDetails">
-        <div className="votes">
-          <button className="upVotes">
-            <span role="img" aria-label="UpVote">👍</span> <span className="hover" aria-labelledby="UpVote">{upVote}</span>
-          </button>
-          <button className="downVotes">
-          <span role="img" aria-label="DownVote">👎</span> <span className="hover" aria-labelledby="DownVote">{downVote}</span>
-          </button>
+    this.handleVote = this.handleVote.bind(this);
+  }
+
+  handleVote(event) {
+    axios.post('http://localhost:3333/API/Vote', {
+      "BOOK_ID": "bookID",
+      "VOTE": "UP"
+    })
+      .then((res) => console.log(res.data.VOTES.UP)
+      ).catch((err) => console.log(err));
+  }
+
+  render() {
+    const book = this.props.results;
+    let bookID = book.BOOK_ID;
+    let bookTitle = book.TITLE;
+    let bookAuthor = book.AUTHOR;
+    let bookISBN = book.ISBN;
+    let bookImage ='https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Clrs3.jpeg/220px-Clrs3.jpeg';
+    let upVote = book.VOTES.UP;
+    let downVote = book.VOTES.DOWN;
+
+    console.log(this.state.response);
+    return (
+      <div className="searchResultContainer">
+        <div className="imageContainer">
+          <img src={bookImage} alt=""/>
         </div>
-        <span className="bookTitle">{bookTitle}</span>
-        <br />
-        <span className="bookAuthor">
-          <span style={{ fontWeight: 700 }}>Authors:</span> {bookAuthor}
-        </span>
-        <br />
-        <span className="bookISBN">
-          <span style={{ fontWeight: 700 }}>ISBN:</span> {bookISBN}
-        </span>
-        <br />
+        <div className="bookDetails">
+          <div className="votes">
+            <button className="upVotes">
+              <a href="#" onClick={this.handleVote}><span role="img" aria-label="UpVote">👍</span> <span className="hover" aria-labelledby="UpVote">{upVote}</span></a>
+            </button>
+            <button className="downVotes">
+            <span role="img" aria-label="DownVote">👎</span> <span className="hover" aria-labelledby="DownVote">{downVote}</span>
+            </button>
+          </div>
+          <span className="bookTitle">{bookTitle}</span>
+          <br />
+          <span className="bookAuthor">
+            <span style={{ fontWeight: 700 }}>Authors:</span> {bookAuthor}
+          </span>
+          <br />
+          <span className="bookISBN">
+            <span style={{ fontWeight: 700 }}>ISBN:</span> {bookISBN}
+          </span>
+          <br />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default SearchResult;
