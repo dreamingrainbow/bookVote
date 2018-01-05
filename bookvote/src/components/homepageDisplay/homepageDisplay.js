5cebcd27d9b5b1f56294fed56a2bc8a09362684a
ubuntu <irishseanvaldivia@gmail.com>
2018-01-04 20:44:33 -0600

    Inching along...

--- bookvote/src/components/homepageDisplay/homepageDisplay.js
+++ bookvote/src/components/homepageDisplay/homepageDisplay.js
@@ -5,14 +5,6 @@ class PageDisplay extends Component {
     return (
       <div className="PageDisplay">
         <div className="pageContainer">
-          <div className="doubleImageStrip">
-            <div className="imageBox" id="books"></div>
-            <div className="imageBox" id="bookSearch"></div>
-          </div>
-          <div className="doubleImageStrip">
-            <div className="imageBox" id="heapsOfBooks"></div>
-            <div className="imageBox" id="mentalHealth"></div>
-          </div>
         </div>
       </div>
     )
