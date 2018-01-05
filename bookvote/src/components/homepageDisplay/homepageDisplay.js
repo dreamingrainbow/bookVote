599ceeed825c46354b15e33b1fd532cb9de0846a
ubuntu <irishseanvaldivia@gmail.com>
2018-01-04 19:21:46 -0600

    Never give up.

--- bookvote/src/components/homepageDisplay/homepageDisplay.js
+++ bookvote/src/components/homepageDisplay/homepageDisplay.js
@@ -6,12 +6,12 @@ class PageDisplay extends Component {
       <div className="PageDisplay">
         <div className="pageContainer">
           <div className="doubleImageStrip">
-            <div className="imageBox"></div>
-            <div className="imageBox"></div>
+            <div className="imageBox" id="books"></div>
+            <div className="imageBox" id="bookSearch"></div>
           </div>
           <div className="doubleImageStrip">
-            <div className="imageBox"></div>
-            <div className="imageBox"></div>
+            <div className="imageBox" id="heapsOfBooks"></div>
+            <div className="imageBox" id="mentalHealth"></div>
           </div>
         </div>
       </div>
