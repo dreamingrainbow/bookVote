87a8aea45caf2249279c83accc88ceac39ead301
Michael Dennis <michaeladennis@yahoo.com>
2018-01-04 21:53:09 -0700

    Up n Running

--- bookvote/src/components/users/signin.js
+++ bookvote/src/components/users/signin.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { axios } from 'axios';
 import { withRouter, NavLink } from 'react-router-dom';
 
 class SignIn extends Component {
