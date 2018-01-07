66cb940e09fbbd89c8c5f7067a0f9aa4eb5112e0
Michael Dennis <michaeladennis@yahoo.com>
2018-01-07 14:59:40 -0700

    updating Categories.js

--- bookvote/src/components/search/Categories.js
+++ bookvote/src/components/search/Categories.js
@@ -95,130 +95,363 @@ export const Categories = [
         name:"Computer Science",
         path:"/Computer Science".replace(/\s/g, "-"),
         subcategories:[
-            "Algorithms",
-            "Artificial Intelligence",
-            "Database Storage & Design",
-            "Graphics & Visualization",
-            "Networking",
-            "Object-Oriented Software Design",
-            "Operating Systems",
-            "Programming Languages",
-            "Software Design & Engineering"
+            {
+                name : "Algorithms",
+                path : "/Algorithms",
+            },
+            {
+                name : "Artificial Intelligence",
+                path : "/Artificial Intelligence".replace(/\s/g, "-"),
+            },
+            {
+                name : "Database Storage & Design",
+                path : "/Database Storage & Design".replace(/\s/g, "-"),
+            },
+            {
+                name : "Graphics & Visualization",
+                path : "/Graphics & Visualization".replace(/\s/g, "-"),
+            },
+            {
+                name : "Networking",
+                path : "/Networking",
+            },
+            {
+                name : "Object-Oriented Software Design",
+                path : "/Object-Oriented Software Design".replace(/\s/g, "-")
+            },
+            {
+                name : "Operating Systems",
+                path : "/Operating Systems".replace(/\s/g, "-")
+            },
+            {
+                name : "Programming Languages",
+                path : "/Programming Languages".replace(/\s/g, "-")
+            },
+            {
+                name : "Software Design & Engineering",
+                path : "/Software Design & Engineering".replace(/\s/g, "-")
+            },
         ]
     },
     {
         name:"Education",
         path:"/Education",
         subcategories:[
-            "Administration",
-            "Counseling",
-            "Curriculum & Instruction",
-            "Educational Philosophy",
-            "Elementary Education",
-            "History & Theory",
-            "Secondary Education",
-            "Special Education"
+            {
+                name : "Administration",
+                path : "/Administration",
+            },
+
+            {
+                name : "Counseling",
+                path : "/Counseling",
+            },
+            {
+                name : "Curriculum & Instruction",
+                path : "/Curriculum & Instruction".replace(/\s/g, "-"),
+            },
+
+
+            {
+                name : "Educational Philosophy",
+                path : "/Educational Philosophy".replace(/\s/g, "-"),
+            },
+            {
+                name : "Elementary Education",
+                path : "/Elementary Education".replace(/\s/g, "-"),
+            },
+
+            {
+                name : "History & Theory",
+                path : "/History & Theory".replace(/\s/g, "-"),
+            },
+            {
+                name : "Secondary Education",
+                path : "/Secondary Education".replace(/\s/g, "-"),
+            },
+            {
+                name : "Special Education",
+                path : "/Special Education".replace(/\s/g, "-")
+            },
         ]
     },
     {
         name:"Engineering",
         path:"/Engineering",
         subcategories:[
-            "Aeronautical Engineering",
-            "Chemical Engineering",
-            "Civil Engineering",
-            "Electrical & Electronic Engineering",
-            "Environmental Engineering",
-            "Industrial Engineering",
-            "Mechanical Engineering",
-            "Nuclear Engineering"
+            {
+                name : "Aeronautical Engineering",
+                path : "/Aeronautical Engineering".replace(/\s/g, "-"),
+            },
+            {
+                name : "Chemical Engineering",
+                path : "/Chemical Engineering".replace(/\s/g, "-"),
+            },
+            {
+                name : "Civil Engineering",
+                path : "/Civil Engineering".replace(/\s/g, "-"),
+            },
+            {
+                name : "Electrical & Electronic Engineering",
+                path : "/Electrical & Electronic Engineering".replace(/\s/g, "-"),
+            },
+            {
+                name : "Environmental Engineering",
+                path : "/Environmental Engineering".replace(/\s/g, "-"),
+            },
+            {
+                name : "Industrial Engineering",
+                path : "/Industrial Engineering".replace(/\s/g, "-"),
+            },
+            
+            {
+                name : "Mechanical Engineering",
+                path : "/Mechanical Engineering".replace(/\s/g, "-"),
+            },
+            {
+                name : "Nuclear Engineering",
+                path : "/Nuclear Engineering".replace(/\s/g, "-")
+            },
         ]
     },
     {
         name:"Humanities",
         path:"/Humanities",
         subcategories:[
-            "Architecture",
-            "Art History",
-            "Design",
-            "Foreign Languages",
-            "History",
-            "Library & Information Science",
-            "Linguistics",
-            "Literature",
-            "Performing Arts",
-            "Philosophy",
-            "Religious Studies",
-            "Visual Arts"
+            {
+                name : "Architecture",
+                path : "/Architecture",
+            },
+            {
+                name : "Art History",
+                path : "/Art History".replace(/\s/g, "-"),
+            },
+            {
+                name : "Design",
+                path : "/Design",
+            },
+            {
+                name : "Foreign Languages",
+                path : "/Foreign Languages".replace(/\s/g, "-"),
+            },
+            {
+                name : "History",
+                path : "/History",
+            },
+            {
+                name : "Library & Information Science",
+                path : "/Library & Information Science".replace(/\s/g, "-"),
+            },
+            {
+                name : "Linguistics",
+                path : "/Linguistics",
+            },
+            {
+                name : "Literature",
+                path : "/Literature",
+            },
+            {
+                name : "Performing Arts",
+                path : "/Performing Arts".replace(/\s/g, "-"),
+            },
+            {
+                name : "Philosophy",
+                path : "/Philosophy",
+            },
+            {
+                name : "Religious Studies",
+                path : "/Religious Studies".replace(/\s/g, "-"),
+            },
+            {
+                name : "Visual Arts",
+                path : "/Visual Arts".replace(/\s/g, "-")
+            },
         ]
     },
     {
         name:"Law",
         path:"/Law",
         subcategories:[
-            "Business Law",
-            "Constitutional Law",
-            "Criminal Law",
-            "Legal Reference",
-            "Tax Law"
+            {
+                name : "Business Law",
+                path : "/Business Law".replace(/\s/g, "-"),
+            },
+            {
+                name : "Constitutional Law",
+                path : "/Constitutional Law".replace(/\s/g, "-"),
+            },
+            {
+                name : "Criminal Law",
+                path : "/Criminal Law".replace(/\s/g, "-"),
+            },
+            {
+                name : "Legal Reference",
+                path : "/Legal Reference".replace(/\s/g, "-"),
+            },
+            {
+                name : "Tax Law",
+                path : "/Tax Law".replace(/\s/g, "-"),
+            },
         ]
     },
     {
         name:"Medicine & Health Sciences",
         path:"/Medicine & Health Sciences".replace(/\s/g, "-"),
         subcategories:[
-            "Administration & Policy",
-            "Allied Health Services",
-            "Alternative Medicine",
-            "Dentistry",
-            "Medicine",
-            "Nursing",
-            "Reference",
-            "Research",
-            "Test Preparation & Review",
-            "Veterinary Medicine"     
+            {
+                name : "Administration & Policy",
+                path : "/Administration & Policy".replace(/\s/g, "-"),
+            },
+            {
+                name : "Allied Health Services",
+                path : "/Allied Health Services".replace(/\s/g, "-"),
+            },
+            {
+                name : "Alternative Medicine",
+                path : "/Alternative Medicine".replace(/\s/g, "-"),
+            },
+            {
+                name : "Dentistry",
+                path : "/Dentistry",
+            },
+            {
+                name : "Medicine",
+                path : "/Medicine",
+            },
+            {
+                name : "Nursing",
+                path : "/Nursing",
+            },
+            {
+                name : "Reference",
+                path : "/Reference",
+            },
+            {
+                name : "Research",
+                path : "/Research",
+            },
+            {
+                name : "Test Preparation & Review",
+                path : "/Test Preparation & Review".replace(/\s/g, "-"),
+            },
+            {
+                name : "Veterinary Medicine", 
+                path : "/Veterinary Medicine".replace(/\s/g, "-")
+            },
         ]
     },
     {
         name:"Reference",
         path:"/Reference",
         subcategories:[
-            "Atlases",
-            "Bibliographies & Indexes",
-            "Dictionaries",
-            "Encyclopedias",
-            "Writing Skills"            
+            {
+                name : "Atlases",
+                path : "/Atlases",
+            },
+            {
+                name : "Bibliographies & Indexes",
+                path : "/Bibliographies & Indexes".replace(/\s/g, "-"),
+            },
+            {
+                name : "Dictionaries",
+                path : "/Dictionaries",
+            },
+            {
+                name : "Encyclopedias",
+                path : "/Encyclopedias",
+            },
+            {
+                name : "Writing Skills",
+                path : "/Writing Skills".replace(/\s/g, "-"),
+            },         
         ]
     },
     {
         name:"Science & Mathematics",
         path:"/Science & Mathematics".replace(/\s/g, "-"),
         subcategories:[
-            "Agriculture",
-            "Astronomy & Astrophysics",
-            "Biology & Life Sciences",
-            "Chemistry",
-            "Earth Sciences",
-            "Environmental Studies",
-            "Mathematics",
-            "Mechanics",
-            "Physics"
+            {
+                name : "Agriculture",
+                path : "/Agriculture",
+            },
+            {
+                name : "Astronomy & Astrophysics",
+                path : "/Astronomy & Astrophysics".replace(/\s/g, "-"),
+            },
+            {
+                name : "Biology & Life Sciences",
+                path : "/Biology & Life Sciences".replace(/\s/g, "-"),
+            },
+            {
+                name : "Chemistry",
+                path : "/Chemistry",
+            },
+            {
+                name : "Earth Sciences",
+                path : "/Earth Sciences".replace(/\s/g, "-"),
+            },
+            {
+                name : "Environmental Studies",
+                path : "/Environmental Studies".replace(/\s/g, "-"),
+            },
+            {
+                name : "Mathematics",
+                path : "/Mathematics"
+            },
+            {
+                name : "Mechanics",
+                path : "/Mechanics"
+            },
+            {
+                name : "Physics",
+                path : "/Physics"
+            },
         ]
     },
     {
         name:"Social Sciences",
         path:"/Social Sciences".replace(/\s/g, "-"),
         subcategories:[
-            "Anthropology",
-            "Archaeology",
-            "Criminology",
-            "Gay & Lesbian Studies",
-            "Gender Studies",
-            "Geography",
-            "Military Sciences",
-            "Political Science",
-            "Psychology",
-            "Sociology"            
+            {
+                name : "Anthropology",
+                path : "/Anthropology",
+            },
+            {
+                name : "Archaeology",
+                path : "/Archaeology",
+            },
+            {
+                name : "Criminology",
+                path : "/Criminology",
+            },
+            {
+                name : "Gay & Lesbian Studies",
+                path : "/Gay & Lesbian Studies".replace(/\s/g, "-"),
+            },
+            {
+                name : "Gender Studies",
+                path : "/Gender Studies".replace(/\s/g, "-"),
+            },
+            {
+                name : "Geography",
+                path : "/Geography",
+            },
+            {
+                name : "Military Sciences",
+                path : "/Military Sciences".replace(/\s/g, "-"),
+            },
+            {
+                name : "Political Science",
+                path : "/Political Science".replace(/\s/g, "-"),
+            },
+            {
+                name : "Psychology",
+                path : "/Psychology",
+            },
+            {
+                name : "Sociology", 
+                path : "/Sociology",
+            },
         ]
     },
     {
